144362e232e20e5fc27acf1b93a6a01bf133e10d
RecentRichRail <80782821+RecentRichRail@users.noreply.github.com>
2024-05-31 23:56:46 -0500

    Refactor banana clicker authentication and update button image

--- resources/banana.py
+++ resources/banana.py
@@ -5,9 +5,9 @@ bp = Blueprint('banana_clicker', __name__, url_prefix='/internal/banana')
 
 @bp.route('/')
 def index():
-    response = requests.post(f"http://{current_app.mysql_database_api}/apiv1/games/banana_clicker/is_user_allowed", json={"jwt": request.cookies.get(current_app.short_session_cookie_name)})
-    is_allowed = response.json()
-    if is_allowed['allowed'] == False:
-        print(is_allowed)
+    response = requests.post(f"http://{current_app.authentication_server}/apiv1/auth/get_user_info", json={"jwt": request.cookies.get(current_app.short_session_cookie_name)})
+    user_info = response.json()
+    if user_info['valid'] == False:
         return redirect('/internal/search')
-    return render_template('index.html', user_id=is_allowed['id'])
+    elif user_info['valid']:
+        return render_template('index.html', user_id=user_info['sub'])
